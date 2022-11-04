b119ad0a5762fcc7063d1d1855b6152029201a56
IloveNooodles <mgarebaldhie80@gmail.com>
2022-11-05 03:11:09 +0700

    feat: implement crc32

--- lib/segment.py
+++ lib/segment.py
@@ -1,21 +1,16 @@
 import struct
 
-# Constants 
-SYN_FLAG = 0b0
-ACK_FLAG = 0b0
-FIN_FLAG = 0b0
+from constant import ACK_FLAG, FIN_FLAG, SYN_FLAG
+
 
 class SegmentFlag:
-    def __init__(self, flag : bytes):
+    def __init__(self, flag: bytes):
         # Init flag variable from flag byte
         pass
 
     def get_flag_bytes(self) -> bytes:
         # Convert this object to flag in byte form
         pass
-
-
-
 
 
 class Segment:
@@ -34,17 +29,15 @@ class Segment:
         # Calculate checksum here, return checksum result
         pass
 
-
     # -- Setter --
-    def set_header(self, header : dict):
+    def set_header(self, header: dict):
         pass
 
-    def set_payload(self, payload : bytes):
+    def set_payload(self, payload: bytes):
         pass
 
-    def set_flag(self, flag_list : list):
+    def set_flag(self, flag_list: list):
         pass
-
 
     # -- Getter --
     def get_flag(self) -> SegmentFlag:
@@ -56,16 +49,14 @@ class Segment:
     def get_payload(self) -> bytes:
         pass
 
-
     # -- Marshalling --
-    def set_from_bytes(self, src : bytes):
+    def set_from_bytes(self, src: bytes):
         # From pure bytes, unpack() and set into python variable
         pass
 
     def get_bytes(self) -> bytes:
         # Convert this object to pure bytes
         pass
-
 
     # -- Checksum --
     def valid_checksum(self) -> bool:
