f97849fabb61acc6287fcbd48231a3809ae8be1a
IloveNooodles <mgarebaldhie80@gmail.com>
2022-11-05 23:00:16 +0700

    feat: finish implement connection

--- lib/connection.py
+++ lib/connection.py
@@ -1,19 +1,26 @@
 import socket
+
+from .constant import DEFAULT_IP, DEFAULT_PORT, SEGMENT_SIZE, TIMEOUT
 from .segment import Segment
 
-class Connection:
-    def __init__(self, ip : str, port : int):
-        # Init UDP socket
-        pass
 
-    def send_data(self, msg : Segment, dest : ("ip", "port")):
+class Connection:
+    def __init__(self, ip: str, port: int):
+        self.ip = ip
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((ip, port))
+
+    def send_data(
+        self, msg: Segment, dest: ("ip", "port") = (DEFAULT_IP, DEFAULT_PORT)
+    ):
         # Send single segment into destination
-        pass
+        self.socket.sendto(msg, dest)
 
     def listen_single_segment(self) -> Segment:
         # Listen single UDP datagram within timeout and convert into segment
-        pass
+        self.socket.settimeout(TIMEOUT)
+        self.socket.recvfrom(SEGMENT_SIZE)
 
     def close_socket(self):
-        # Release UDP socket
-        pass
+        self.socket.close()
